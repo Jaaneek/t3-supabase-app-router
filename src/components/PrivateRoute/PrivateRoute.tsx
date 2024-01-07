ba7d6b7192da59984d6db7845d26cd2c265e4ef8
Joseph Burdick <josephdburdick@gmail.com>
2024-01-06 23:59:34 -0500

    Render null within PrivateRouteBase. Closes #12

--- src/components/PrivateRoute/PrivateRoute.tsx
+++ src/components/PrivateRoute/PrivateRoute.tsx
@@ -5,7 +5,8 @@ import { PrivateRouteBase } from "./PrivateRouteBase";
 export const PrivateRoute = async ({ children }: PropsWithChildren) => {
   const user = await getServerUser();
 
-  if (!user) return null; // Prevent server side render of authorized page
+  // prevent server side render of authorized page
+  const view = !user ? null : children;
 
-  return <PrivateRouteBase>{children}</PrivateRouteBase>;
+  return <PrivateRouteBase>{view}</PrivateRouteBase>;
 };
